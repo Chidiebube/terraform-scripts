ec713c9ed6feaaa7d465244f5b258792e5a879ea
Chidiebube Amos <chidiebubeamos@gmail.com>
2017-02-22 18:35:16 +0100

    updated config files

--- digital_ocean/digitalocean_create_record.tf
+++ digital_ocean/digitalocean_create_record.tf
@@ -1,15 +1,15 @@
-/*# Create a new domain 
+# Create a new domain 
 resource "digitalocean_domain" "default" {
-    name = "amosbube.com"
+    name = "coundigital.com"
     ip_address = "${digitalocean_droplet.terraform.ipv4_address}"
-}*/
+}
 
 
 # Add a record to the domain
 resource "digitalocean_record" "terraform5" {
-    domain = "terraform.skyrunmall.com"
-    #domain = "${digitalocean_domain.default.name}"
-    type = "A"
-    name = "terraformrecor" 
-    value = "46.101.205.36"  #pointing to the ip of an existing droplet on Digital Ocean
+    #domain = "skyrunmall.com"
+    domain = "${digitalocean_domain.default.name}"
+    type = "CNAME"
+    name = "terraformrecord" 
+    value = "skrunmall.com."  #pointing to the ip of an existing droplet on Digital Ocean
 }
